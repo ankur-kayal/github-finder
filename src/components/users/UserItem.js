fa6b7783c42eee91bd8ee3289603aa5fd7441cf1
Ankur Kayal <ankurkayal1234@gmail.com>
2020-10-14 19:54:19 +0530

    Users Component Added

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserItem = (props) => {
+  const { avatar_url, login, html_url } = props.user;
+
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  users: PropTypes.object.isRequired,
+};
+
+export default UserItem;
