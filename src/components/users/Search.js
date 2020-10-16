2f3892d0d147217f3923ae1418d685e69bf06b3b
Ankur Kayal <ankurkayal1234@gmail.com>
2020-10-17 00:15:01 +0530

    Searching of user names added along with necessary alerts

---
+++ src/components/users/Search.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
+
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something!', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-block btn-dark'
+          />
+          {this.props.showClear && (
+            <button
+              onClick={this.clearUsers}
+              className='btn btn-light btn-block'
+            >
+              Clear
+            </button>
+          )}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
